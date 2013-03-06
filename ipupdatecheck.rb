5f1d52f6ea3e8aedb328993af11a34255ac61bdd
Bernd Plontsch <bernd.plontsch@gmail.com>
2013-03-06 18:30:57 +0100

    Using Resolv library and Ruby 1.9.2

--- ipupdatecheck.rb
+++ ipupdatecheck.rb
@@ -1,22 +1,28 @@
 #!/usr/bin/ruby
 
-IP = ARGV[0]
-URL = ARGV[1]
+require 'Resolv'
 
-if (ARGV.count==2)
-	puts "Start checking for IP change. [IP] #{ARGV[0]} [URL] #{ARGV[1]}"
+IP = Resolv.getaddress ARGV[0]
+
+puts "IP #{IP}"
+
+# IP = ARGV[0]
+URL = ARGV[0]
+
+if (ARGV.count==1)
+	puts "Start checking for IP change. [IP] #{IP} [URL] #{URL}"
 else
-	puts "Usage: ipupdatecheck <current IP> <url>"
+	puts "Usage: ipupdatecheck <url>"
 end
 
 p = IO.popen("ping -i 10 #{URL}")
 p.each_line do |l|
   timestamp = Time.now.strftime("%H:%M:%S")
   if(l.scan("#{IP}").empty?)
-  	puts "#{timestamp} IP Changed ‣ #{IP}"
+  	puts "#{timestamp} ‣ #{IP} (Updated!)"
     system("terminal-notifier -message \"DNS Changed\" -open \"http://#{URL}\"")
   else
 	timestamp = Time.now.strftime("%H:%M:%S")
-	puts "#{timestamp}"   	
+	puts "#{timestamp} • #{IP} (Original)"   	
   end
 end
